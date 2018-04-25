34afebcbf881169f79c404c907f71b57353d1b95
zhanghl-k <zhanghl-k@glodon.com>
2018-04-25 11:10:18 +0800

    black body radiation

--- test/math.spec.ts
+++ test/math.spec.ts
@@ -1,6 +1,6 @@
 import * as math from 'mathjs'
 describe(`https://en.wikipedia.org/wiki/Black-body_radiation`, () => {
     it(`{\displaystyle B_{\nu }(T)={\frac {2h\nu ^{3}}{c^{2}}}{\frac {1}{e^{\frac {h\nu }{kT}}-1}},}`, () => {
-        expect(math.eval(`{\displaystyle B_{\nu }(T)={\frac {2h\nu ^{3}}{c^{2}}}{\frac {1}{e^{\frac {h\nu }{kT}}-1}},}`)).toEqual()
+        expect(math.eval(`B_(T)=2*h*v ^3/c^2*1/(e^(h*v/(k*T))-1)`)).toEqual(expect.any(Function))
     });
 });
