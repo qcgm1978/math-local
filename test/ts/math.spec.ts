1f9e2ca8ba0da1b514babe8fab75a371fc92b06d
zhanghl-k <zhanghl-k@glodon.com>
2018-04-25 14:23:17 +0800

    uniform distribution

--- test/ts/math.spec.ts
+++ test/ts/math.spec.ts
@@ -4,3 +4,13 @@ describe(`https://en.wikipedia.org/wiki/Black-body_radiation`, () => {
         expect(math.eval(`B_(T)=2*h*v ^3/c^2*1/(e^(h*v/(k*T))-1)`)).toEqual(expect.any(Function))
     });
 });
+describe(`http://mathjs.org/docs/reference/functions/random.html`, () => {
+    it(`Return a random number larger or equal to min and smaller than max using a uniform distribution.`, () => {
+        expect(math.random()).toBeLessThan(1)
+        expect(math.random(100)).toBeLessThan(100);    // returns a random number between 0 and 100
+        expect(math.random(30, 40)).toBeLessThan(40); // returns a random number between 30 and 40
+        expect(math.random([2, 3]).length).toEqual(2); // returns a 2x3 matrix with random numbers between 0 and 1
+        expect(math.random([2, 3])[0].length).toEqual(3); // returns a 2x3 matrix with random numbers between 0 and 1
+
+    });
+});
