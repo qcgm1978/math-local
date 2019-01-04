fdb165b1c776e06fd34ded450c9f5a1bb6ba4da2
qcgm1978 <zhanghl@strongunion.com.cn>
2019-01-04 14:37:54 +0800

    Mock

--- test/jest/lib.spec.js
+++ test/jest/lib.spec.js
@@ -1,4 +1,15 @@
 const _ = require('lodash')
+const Mock = require('mockjs')
+it(`Mock.js is a simulation data generator to help the front-end to develop and prototype separate from the back-end progress and reduce some monotony particularly while writing automated tests.`, () => {
+    const data = Mock.mock({
+        // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+        'list|1-10': [{
+            // 属性 id 是一个自增数，起始值为 1，每次增 1
+            'id|+1': 1
+        }]
+    })
+    expect(data.list.length).toBeLessThan(11).toBeGreaterThan(0)
+});
 it(`chunk`, () => {
     let arr = _.chunk([1, 2, 3])
     expect(arr).toEqual([[1], [2], [3]])
