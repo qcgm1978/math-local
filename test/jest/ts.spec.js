1c0c3709394ee6fecc9a1b8d1c5c7b7b597829e1
mac <mac@macdeMac-mini.lan>
2019-01-27 15:37:46 +0800

    But what about concepts? Yeah, most of them are impossible to be "found" in a very short time and require a significant amount of learning effort.

--- test/jest/ts.spec.js
+++ test/jest/ts.spec.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
-it(``, async function (done) {
+it(`But what about concepts? Yeah, most of them are impossible to be "found" in a very short time and require a significant amount of learning effort.`, async function (done) {
     const a = tf.data.array([1, 2, 3, 4, 5, 6, 7, 8]).batch(4);
     await a.forEach(e => {
         expect(e.shape).toEqual([4])
