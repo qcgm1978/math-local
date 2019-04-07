6e961d7958bc1895247c89604c38563766928d26
张红亮 <zhanghongliang@zhanghoiangsAir.lan>
2019-04-07 22:32:04 +0800

    jhd add

---
+++ test/jest/JHD.spec.js
@@ -0,0 +1,6 @@
+const { idCardValid } = require('../../JHD/idCardValidation')
+it(`身份证号合法性验证`, () => {
+    expect(idCardValid(18888)).toBeFalsy()
+    expect(idCardValid(18602481788)).toBeFalsy()
+    expect(idCardValid(211226197703220218)).toBeFalsy()
+});
